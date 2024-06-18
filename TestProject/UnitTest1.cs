d402ad27e7e1e272b92b5500de327bbfd8e7d12e
operationd8979 <operationddd@gmail.com>
2024-06-18 07:16:31 +0700

    update init test unit

---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,33 @@
+
+
+using Microsoft.Extensions.Logging;
+using Moq;
+using TipRecipe.Interfaces;
+using TipRecipe.Services;
+
+namespace TestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Mock<IDishRepository> mockDishRepository = new Mock<IDishRepository>();
+        Mock<IIngredientRepository> mockIngredientRepository = new Mock<IIngredientRepository>();
+        Mock<ITypeDishRepository> mockTypeDishRepository = new Mock<ITypeDishRepository>();
+        Mock<ILogger<CachingFileService>> mockLogger = new Mock<ILogger<CachingFileService>>();
+
+        DishService dishService;
+
+
+
+        [TestMethod]
+        public void GetCountDishesAsync()
+        {
+            Mock<CachingFileService> mockCachingFileService = new Mock<CachingFileService>("testSample",mockLogger.Object);
+            Mock<CachedRatingScoreService> mockCachedRatingService = new Mock<CachedRatingScoreService>(mockCachingFileService.Object);
+            dishService = new DishService(mockDishRepository.Object, mockIngredientRepository.Object, mockTypeDishRepository.Object, mockCachedRatingService.Object);
+            mockDishRepository.Setup(x => x.GetCountAsync("test")).ReturnsAsync(5);
+            var result = dishService.GetCountDishesAsync("test").Result;
+            Assert.AreEqual(5, result);
+        }
+    }
+}
